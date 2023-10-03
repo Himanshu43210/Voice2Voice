544ae2084ffd0c989afb46f206a5a249512080f6
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-10-03 19:24:56 +0530

    structure update

--- constants/filler_mapping.py
+++ constants/filler_mapping.py
@@ -76,9 +76,9 @@ def classify_and_play_audio(sentence):
     # Classify a new sentence
     audio_file = loaded_model.predict([sentence])[0]
     # Path to the audio file
-    audio_path = (
-        f"G:/Workspace/git/AI/Voice2Voice/components/audio_fillers/{audio_file}"
-    )
+    # 
+
+    audio_path = f"./assets/audio_fillers/{audio_file}"
     # Play the audio file
     play_audio(audio_path)
 
--- main.py
+++ main.py
@@ -23,8 +23,10 @@ nest_asyncio.apply()
 from stream_text_to_audio import stream_text_to_audio
 
 sys.path.append("./components")
+sys.path.append("./constants")
 from speech_to_text import transcribe_stream
 from filler_mapping import classify_and_play_audio, refresh_learning_data
+from dictionary import phrases_dict
 
 # Load environment variables
 load_dotenv()
@@ -40,6 +42,7 @@ os.environ["PLAYHT_USER_ID"] = PLAYHT_USER_ID
 sys.path.append("./components")
 
 
+
 class DictionaryCallback(BaseCallbackHandler):
     def __init__(self, q):
         self.q = q
@@ -48,56 +51,57 @@ class DictionaryCallback(BaseCallbackHandler):
         self.thread = Thread(target=self.start_loop, args=(self.loop,))
         self.thread.start()
 
-        self.phrases_dict = {
-            "That 's great to hear!": "Thatsgreattohear.wav",
-            "The Google Pixel phones are an excellent choice.": "TheGooglePixelphonesareanexcellentchoice.wav",
-            "They offer a fantastic camera experience and a pure Android operating system.": "TheyofferafantasticcameraexperienceandapureAndroidoperatingsystem.wav",
-            "Is there anything specific you 're looking for in a smartphone?": "Isthereanythingspecificyourelookingforinasmartphone.wav",
-            "Absolutely!": "Absolutely.wav",
-            "Camera quality is one of the standout features of Google Pixel phones.": "CameraqualityisoneofthestandoutfeaturesofGooglePixelphones.wav",
-            "They are known for their exceptional camera capabilities, especially in low -light conditions.": "Theyareknownfortheirexceptionalcameracapabilitiesespeciallyinlowlightconditions.wav",
-            "The Google Pixel 6, in particular, comes with a 50 megapixel main camera and Google 's Night Sight technology, which ensures clear and vibrant photos even in challenging lighting situations.": "TheGooglePixel6inparticularcomeswitha50megapixelmaincameraandGooglesNightSighttechnologywhichensuresclearandvibrantphotoseveninchallenginglightingsituations.wav",
-            "If camera quality is a top priority for you, the Google Pixel 6 would be an excellent choice.": "IfcameraqualityisatoppriorityforyoutheGooglePixel6wouldbeanexcellentchoice.wav",
-            "Would you like to book an appointment to visit our showroom and experience the Google Pixel 6 firsthand?": "WouldyouliketobookanappointmenttovisitourshowroomandexperiencetheGooglePixel6firsthand.wav",
-            "That 's great to hear!": "Thatsgreattohear.wav",
-            "We have a wide range of Google Pixel phones available in our store.": "WehaveawiderangeofGooglePixelphonesavailableinourshowroom.wav",
-            "When would you like to visit us?": "Whenwouldyouliketovisitus.wav",
-            "Can I have your name and contact number to schedule an appointment for you?": "CanIhaveyournameandcontactnumbertoscheduleanappointmentforyou.wav",
-            "Great, John!": "GreatJohn.wav",
-            "Thank you for providing your name and contact number.": "Thankyouforprovidingyournameandcontactnumber.wav",
-            "I have scheduled an appointment for you to visit our shop and experience the Google Pixel phones firsthand.": "IhavescheduledanappointmentforyoutovisitourshopandexperiencetheGooglePixelphonesfirsthand.wav",
-            "Our team at Gadget Hub is excited to assist you in finding the perfect phone with excellent camera quality.": "OurteamatGadgetHubisexcitedtoassistyouinfindingtheperfectphonewithexcellentcameraquality.wav",
-            "We look forward to seeing you soon!": "Welookforwardtoseeingyousoon.wav",
-            "Have a great day!": "Haveagreatday.wav",
-            'Great!': 'Great.wav',
-            'I can definitely help you with that.': 'Icandefinitelyhelpyouwiththat.wav',
-            'What specific features are you looking for in a phone?': 'Whatspecificfeaturesareyoulookingforinaphone.wav',
-            'The Google Pixel is known for its exceptional camera quality.': 'TheGooglePixelisknownforitsexceptionalcameraquality.wav',
-            'It has a high-resolution camera that captures stunning photos with great detail and vibrant colors.': 'Ithasahighresolutioncamerathatcapturesstunningphotoswithgreatdetailandvibrantcolors.wav',
-            "You 'll be able to take professional-looking photos with ease.": 'Youllbeabletotakeprofessionallookingphotoswithease.wav',
-            'Would you like to visit our showroom to experience the camera firsthand?': 'Wouldyouliketovisitourshowroomtoexperiencethecamerafirsthand.wav',
-            'Can I have your name and contact number to schedule an appointment for you to visit our shop?': 'CanIhaveyournameandcontactnumbertoscheduleanappointmentforyoutovisitourshop.wav',
-            "I 'd be happy to help you find the perfect phone.": 'Idbehappytohelpyoufindtheperfectphone.wav',
-            "Do you have any specific features in mind that you 're looking for in a phone, or are you open to suggestions?": 'Doyouhaveanyspecificfeaturesinmindthatyourelookingforinaphoneorareyouopentosuggestions.wav',
-            'It has a high-resolution camera with advanced features like Night Sight and Portrait Mode.': 'IthasahighresolutioncamerawithadvancedfeatureslikeNightSightandPortraitMode.wav', 
-            "You 'll be able to capture stunning photos and videos with great clarity and detail.": 'Youllbeabletocapturestunningphotosandvideoswithgreatclarityanddetail.wav',
-            'Thank you, John.': 'ThankyouJohn.wav',
-            "I 've scheduled your personal demo for this Saturday.": 'IvescheduledyourpersonaldemoforthisSaturday.wav',
-            "If you have any questions, please don 't hesitate to contact us.": 'Ifyouhaveanyquestionspleasedonthesitatetocontactus.wav',
-            'Have a great day!': 'Haveagreatday.wav',
-            "I 'm glad to hear that you're interested in purchasing a new phone.": 'Imgladtohearthatyoureinterestedinpurchasinganewphone.wav',
-            'The Google Pixel is an excellent choice.': 'TheGooglePixelisanexcellentchoice.wav',
-            'Can you please let me know what specific features you are looking for in a phone?': 'Canyoupleaseletmeknowwhatspecificfeaturesyouarelookingforinaphone.wav',
-            'This will help me provide you with the best recommendations and information.': 'Thiswillhelpmeprovideyouwiththebestrecommendationsandinformation.wav',
-            'I understand that you are mainly concerned about the quality of the camera.': 'Iunderstandthatyouaremainlyconcernedaboutthequalityofthecamera.wav',
-            'The Google Pixel phones are known for their exceptional camera capabilities.': 'TheGooglePixelphonesareknownfortheirexceptionalcameracapabilities.wav',
-            'They have advanced camera features like Night Sight, which allows you to capture stunning low-light photos, and Portrait Mode, which creates professional-looking photos with a blurred background effect.': 'TheyhaveadvancedcamerafeatureslikeNightSightwhichallowsyoutocapturestunninglowlightphotosandPortraitModewhichcreatesprofessionallookingphotoswithablurredbackgroundeffect.wav',
-            "If you 're interested, I would recommend visiting our shop to experience the Google Pixel's camera firsthand.": 'IfyoureinterestedIwouldrecommendvisitingourshoptoexperiencetheGooglePixelscamerafirsthand.wav',
-            'Would you like to schedule an appointment?': 'Wouldyouliketoscheduleanappointment.wav',
-            'Great choice!': 'Greatchoice.wav',
-            "We 'd be happy to give you a hands-on demo.": 'Wedbehappytogiveyouahandsondemo.wav',
-            'Please let me know your name and contact number so that I can book an appointment for you.': 'PleaseletmeknowyournameandcontactnumbersothatIcanbookanappointmentforyou.wav', 
-        }
+        self.phrases_dict = phrases_dict
+        # self.phrases_dict = {
+        #     "That 's great to hear!": "Thatsgreattohear.wav",
+        #     "The Google Pixel phones are an excellent choice.": "TheGooglePixelphonesareanexcellentchoice.wav",
+        #     "They offer a fantastic camera experience and a pure Android operating system.": "TheyofferafantasticcameraexperienceandapureAndroidoperatingsystem.wav",
+        #     "Is there anything specific you 're looking for in a smartphone?": "Isthereanythingspecificyourelookingforinasmartphone.wav",
+        #     "Absolutely!": "Absolutely.wav",
+        #     "Camera quality is one of the standout features of Google Pixel phones.": "CameraqualityisoneofthestandoutfeaturesofGooglePixelphones.wav",
+        #     "They are known for their exceptional camera capabilities, especially in low -light conditions.": "Theyareknownfortheirexceptionalcameracapabilitiesespeciallyinlowlightconditions.wav",
+        #     "The Google Pixel 6, in particular, comes with a 50 megapixel main camera and Google 's Night Sight technology, which ensures clear and vibrant photos even in challenging lighting situations.": "TheGooglePixel6inparticularcomeswitha50megapixelmaincameraandGooglesNightSighttechnologywhichensuresclearandvibrantphotoseveninchallenginglightingsituations.wav",
+        #     "If camera quality is a top priority for you, the Google Pixel 6 would be an excellent choice.": "IfcameraqualityisatoppriorityforyoutheGooglePixel6wouldbeanexcellentchoice.wav",
+        #     "Would you like to book an appointment to visit our showroom and experience the Google Pixel 6 firsthand?": "WouldyouliketobookanappointmenttovisitourshowroomandexperiencetheGooglePixel6firsthand.wav",
+        #     "That 's great to hear!": "Thatsgreattohear.wav",
+        #     "We have a wide range of Google Pixel phones available in our store.": "WehaveawiderangeofGooglePixelphonesavailableinourshowroom.wav",
+        #     "When would you like to visit us?": "Whenwouldyouliketovisitus.wav",
+        #     "Can I have your name and contact number to schedule an appointment for you?": "CanIhaveyournameandcontactnumbertoscheduleanappointmentforyou.wav",
+        #     "Great, John!": "GreatJohn.wav",
+        #     "Thank you for providing your name and contact number.": "Thankyouforprovidingyournameandcontactnumber.wav",
+        #     "I have scheduled an appointment for you to visit our shop and experience the Google Pixel phones firsthand.": "IhavescheduledanappointmentforyoutovisitourshopandexperiencetheGooglePixelphonesfirsthand.wav",
+        #     "Our team at Gadget Hub is excited to assist you in finding the perfect phone with excellent camera quality.": "OurteamatGadgetHubisexcitedtoassistyouinfindingtheperfectphonewithexcellentcameraquality.wav",
+        #     "We look forward to seeing you soon!": "Welookforwardtoseeingyousoon.wav",
+        #     "Have a great day!": "Haveagreatday.wav",
+        #     'Great!': 'Great.wav',
+        #     'I can definitely help you with that.': 'Icandefinitelyhelpyouwiththat.wav',
+        #     'What specific features are you looking for in a phone?': 'Whatspecificfeaturesareyoulookingforinaphone.wav',
+        #     'The Google Pixel is known for its exceptional camera quality.': 'TheGooglePixelisknownforitsexceptionalcameraquality.wav',
+        #     'It has a high-resolution camera that captures stunning photos with great detail and vibrant colors.': 'Ithasahighresolutioncamerathatcapturesstunningphotoswithgreatdetailandvibrantcolors.wav',
+        #     "You 'll be able to take professional-looking photos with ease.": 'Youllbeabletotakeprofessionallookingphotoswithease.wav',
+        #     'Would you like to visit our showroom to experience the camera firsthand?': 'Wouldyouliketovisitourshowroomtoexperiencethecamerafirsthand.wav',
+        #     'Can I have your name and contact number to schedule an appointment for you to visit our shop?': 'CanIhaveyournameandcontactnumbertoscheduleanappointmentforyoutovisitourshop.wav',
+        #     "I 'd be happy to help you find the perfect phone.": 'Idbehappytohelpyoufindtheperfectphone.wav',
+        #     "Do you have any specific features in mind that you 're looking for in a phone, or are you open to suggestions?": 'Doyouhaveanyspecificfeaturesinmindthatyourelookingforinaphoneorareyouopentosuggestions.wav',
+        #     'It has a high-resolution camera with advanced features like Night Sight and Portrait Mode.': 'IthasahighresolutioncamerawithadvancedfeatureslikeNightSightandPortraitMode.wav', 
+        #     "You 'll be able to capture stunning photos and videos with great clarity and detail.": 'Youllbeabletocapturestunningphotosandvideoswithgreatclarityanddetail.wav',
+        #     'Thank you, John.': 'ThankyouJohn.wav',
+        #     "I 've scheduled your personal demo for this Saturday.": 'IvescheduledyourpersonaldemoforthisSaturday.wav',
+        #     "If you have any questions, please don 't hesitate to contact us.": 'Ifyouhaveanyquestionspleasedonthesitatetocontactus.wav',
+        #     'Have a great day!': 'Haveagreatday.wav',
+        #     "I 'm glad to hear that you're interested in purchasing a new phone.": 'Imgladtohearthatyoureinterestedinpurchasinganewphone.wav',
+        #     'The Google Pixel is an excellent choice.': 'TheGooglePixelisanexcellentchoice.wav',
+        #     'Can you please let me know what specific features you are looking for in a phone?': 'Canyoupleaseletmeknowwhatspecificfeaturesyouarelookingforinaphone.wav',
+        #     'This will help me provide you with the best recommendations and information.': 'Thiswillhelpmeprovideyouwiththebestrecommendationsandinformation.wav',
+        #     'I understand that you are mainly concerned about the quality of the camera.': 'Iunderstandthatyouaremainlyconcernedaboutthequalityofthecamera.wav',
+        #     'The Google Pixel phones are known for their exceptional camera capabilities.': 'TheGooglePixelphonesareknownfortheirexceptionalcameracapabilities.wav',
+        #     'They have advanced camera features like Night Sight, which allows you to capture stunning low-light photos, and Portrait Mode, which creates professional-looking photos with a blurred background effect.': 'TheyhaveadvancedcamerafeatureslikeNightSightwhichallowsyoutocapturestunninglowlightphotosandPortraitModewhichcreatesprofessionallookingphotoswithablurredbackgroundeffect.wav',
+        #     "If you 're interested, I would recommend visiting our shop to experience the Google Pixel's camera firsthand.": 'IfyoureinterestedIwouldrecommendvisitingourshoptoexperiencetheGooglePixelscamerafirsthand.wav',
+        #     'Would you like to schedule an appointment?': 'Wouldyouliketoscheduleanappointment.wav',
+        #     'Great choice!': 'Greatchoice.wav',
+        #     "We 'd be happy to give you a hands-on demo.": 'Wedbehappytogiveyouahandsondemo.wav',
+        #     'Please let me know your name and contact number so that I can book an appointment for you.': 'PleaseletmeknowyournameandcontactnumbersothatIcanbookanappointmentforyou.wav', 
+        # }
         self.is_answer_finished = False
         self.timeout = 0.2  # Set your desired timeout value in seconds
         self.timer = None
@@ -116,7 +120,7 @@ class DictionaryCallback(BaseCallbackHandler):
 
     async def play_audio_async(self, file_id):
         print("play_audio_async called")
-        file_path = f"components/audio_files_pixel/{file_id}"
+        file_path = f"assets/audio_files_pixel/{file_id}"
         if os.path.exists(file_path):
             data, samplerate = sf.read(file_path)
             sd.play(data, samplerate)
@@ -242,7 +246,7 @@ def chat_with_user():
 
     opening_line = "Hello I'm Jacob from Gadget Hub. We noticed that you were looking for Google Pixel phone on our website. Are you interested in purchasing a new phone at the moment?"
     print(opening_line)
-    data, fs = sf.read(f"components/audio_files_pixel/intro.wav", dtype="float32")
+    data, fs = sf.read(f"assets/audio_files_pixel/intro.wav", dtype="float32")
     sd.play(data, fs)
     sd.wait()
 
